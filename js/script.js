3832a8c29e6152c03d450c90a4569ec5d65f3f61
L4TTiCe <dheshan.mail@gmail.com>
2019-07-31 11:11:51 +0530

    Added Beautiful front-end to Play the game
    Signed-off-by: L4TTiCe <dheshan.mail@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 let winner = 'User';
-let turnNumber = 1;
 
+// Chooses Random weapon for AI
 function computerTurn() {
-  const weapons = ['Rock', 'Paper', 'Scissors'];
+  const weapons = ['rock', 'paper', 'scissors'];
   const choice = Math.floor((Math.random() * 3));
 
   return weapons[choice];
 }
 
+// Game Logic
 function playRound(playerSelection, computerSelection) {
   let output = '';
 
   if (playerSelection === 'rock') {
     switch (computerSelection) {
-      case 'Rock':
+      case 'rock':
         output = 'Draw';
         winner = 'Draw';
         break;
-      case 'Paper':
+      case 'paper':
         output = 'You Lose! Paper beats Rock';
         winner = 'Computer';
         break;
-      case 'Scissors':
+      case 'scissors':
         output = 'You Win! Rock beats Scissors';
         winner = 'User';
         break;
@@ -30,15 +31,15 @@ function playRound(playerSelection, computerSelection) {
     }
   } else if (playerSelection === 'paper') {
     switch (computerSelection) {
-      case 'Rock':
+      case 'rock':
         output = 'You Win! Paper beats Rock';
         winner = 'User';
         break;
-      case 'Paper':
+      case 'paper':
         output = 'Draw';
         winner = 'Draw';
         break;
-      case 'Scissors':
+      case 'scissors':
         output = 'You Lose! Scissors beat Paper';
         winner = 'Computer';
         break;
@@ -47,15 +48,15 @@ function playRound(playerSelection, computerSelection) {
     }
   } else if (playerSelection === 'scissors') {
     switch (computerSelection) {
-      case 'Rock':
+      case 'rock':
         output = 'You Lose! Rock beats Scissors';
         winner = 'Computer';
         break;
-      case 'Paper':
+      case 'paper':
         output = 'You Win! Scissors beat Paper';
         winner = 'User';
         break;
-      case 'Scissors':
+      case 'scissors':
         output = 'Draw';
         winner = 'Draw';
         break;
@@ -67,26 +68,60 @@ function playRound(playerSelection, computerSelection) {
   return output;
 }
 
-function playerTurn() {
-  const choice = document.getElementById('userChoice').value;
-  return choice;
+let aiScore = 0;
+let userScore = 0;
+
+const user = document.querySelectorAll('.user > *');
+const score = document.querySelector('#score');
+const comment = document.querySelector('#comment');
+const btns = document.querySelectorAll('.center > *');
+
+user.forEach(btn => btn.addEventListener('click', (e) => {
+  const aiBtn = document.querySelector(`.ai > .${computerTurn()}`);
+
+  const decision = playRound(e.target.className, aiBtn.className);
+
+  switch (winner) {
+    case 'User':
+      userScore += 1;
+      break;
+    case 'Computer':
+      aiScore += 1;
+      break;
+    default:
+      break;
+  }
+
+  comment.innerHTML = decision;
+  score.innerHTML = `${userScore} : ${aiScore}`;
+
+  e.target.classList.add('selected-user');
+  aiBtn.classList.add('selected-ai');
+}));
+
+btns.forEach(btn => btn.addEventListener('transitionend', (e) => {
+  e.target.classList.remove('selected-user');
+  e.target.classList.remove('selected-ai');
+}));
+
+function reset() {
+  userScore = 0;
+  aiScore = 0;
+
+  // score.innerHTML = `${userScore} : ${aiScore}`;
+  score.innerHTML = 'RESET';
+  comment.innerHTML = '';
+  comment.innerHTML = 'Right-click resets the Scorecard.';
+  setTimeout(() => {
+    score.innerHTML = `${userScore} : ${aiScore}`;
+    // comment.innerHTML = '';
+  }, 500);
 }
 
-function game() {
-  const playerChoice = playerTurn();
-  const computerChoice = computerTurn();
+// Disable Context menu and redirect it to reset
+const mainContainer = document.querySelector('#container');
+mainContainer.addEventListener('contextmenu', reset);
 
-  const data = {};
-  data.turnNumber = turnNumber;
-  data.user_choice = playerChoice;
-  data.computer_choice = computerChoice;
-  data.winner = winner;
-
-  console.groupCollapsed(`Turn #${turnNumber}`);
-  console.table(data);
-  console.groupEnd();
-
-  const result = playRound(playerChoice, computerChoice);
-  document.getElementById('result').innerHTML = result;
-  turnNumber += 1;
-}
+document.oncontextmenu = function disableContextMenu() {
+  return false;
+};
